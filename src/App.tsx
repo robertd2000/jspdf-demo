37468049df203fe83be85ea2635d3576f245e34c
robert <ext-robert.dabagov@kuper.ru>
2025-02-27 16:12:16 +0300

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import PDFEditor from "./components/Editor";
-import LabelEditor from "./components/LabelEditor";
 
 function App() {
   return (
     <div className='min-h-screen min-w-screen flex flex-col'>
-      {/* <LabelEditor /> */}
       <PDFEditor />
     </div>
   );
